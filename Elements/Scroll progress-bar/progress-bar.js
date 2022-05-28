943259d3b2b470d020fb9fa526e2dd071c1fd040
0036535126 <jakov.jakovac@fer.hr>
2022-05-28 15:21:26 +0200

    Added google analytics and indexing tags

--- Elements/Scroll progress-bar/progress-bar.js
+++ Elements/Scroll progress-bar/progress-bar.js
@@ -1,20 +1,20 @@
-const pageProgressBar = document.querySelector(".progress-bar")
-const showOnPx = 250; // amount of pixels before bar is shown
+const pageProgressBar = document.querySelector(".progress-bar");
+const showOnPx = 10; // amount of pixels before bar is shown
 
 const scrollContainer = () => {
   return document.documentElement || document.body;
 };
 
 document.addEventListener("scroll", () => {
-  const scrolledPercentage = ( scrollContainer().scrollTop / 
-    (scrollContainer().scrollHeight - scrollContainer().clientHeight) ) * 100;
-  
+  const scrolledPercentage =
+    (scrollContainer().scrollTop /
+      (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+    100;
+
   // console.log(scrolledPercentage);
   pageProgressBar.style.width = scrolledPercentage + "%";
-  
+
   if (scrollContainer().scrollTop > showOnPx)
     pageProgressBar.classList.remove("hidden");
-  else
-    pageProgressBar.classList.add("hidden");
-  
+  else pageProgressBar.classList.add("hidden");
 });
