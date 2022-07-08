decd2e05817a6d0528ef173bacb51c4f5c439438
0036535126 <jakov.jakovac@fer.hr>
2022-07-08 17:47:54 +0200

    Changed color of mobile nav to white (bug). Added search functionality (input and toggle buttons + local storage) to updates.

--- js/updates.js
+++ js/updates.js
@@ -1,5 +1,8 @@
 const postTemplate = document.querySelector("[data-post-template]");
-const posts = document.querySelector(".posts");
+const postsContainer = document.querySelector(".posts");
+
+const filterSearch = document.querySelector(".filter__search");
+const filterButtons = document.querySelectorAll(".filter__buttons > button");
 
 fetch("../data/posts.json")
   .then((res) => res.json())
@@ -27,17 +30,15 @@ fetch("../data/posts.json")
 
       //   console.log(postImgElement);
 
-      posts.append(postElement);
+      postsContainer.append(postElement);
     });
   })
   .then(() => {
-    // if url with specified post id (#id)
-    if (window.location.hash.split("#")[1])
-      document
-        .querySelector('[id="' + window.location.hash.split("#")[1] + '"]')
-        .scrollIntoView(true);
-  })
-  .then(() => {
+    // ===== lightbox =====
+    createLightbox();
+
+    // ===== add buttons to expand post if text is overflowing =====
+
     // contains a bug where the scroll height is equal to client height on start of script
     // and because of that, part of the overflowing contant wont be recognized and therefore
     // the containing post will not be able to be overflow
@@ -95,40 +96,159 @@ fetch("../data/posts.json")
           postsArray[index].scrollIntoView(true); // allign to top = true
       }
     }
-  })
-  .then(() => {
-    // lightbox
 
-    const lightbox = document.createElement("div");
-    lightbox.id = "lightbox";
+    // ===== filter posts =====
 
-    document.body.appendChild(lightbox);
+    const tagsArray = postsContainer.querySelectorAll(".post__tag");
+    const datesArray = postsContainer.querySelectorAll(".post__date");
 
-    lightbox.addEventListener("click", (e) => {
-      if (e.target !== e.currentTarget) return;
+    // if exists retrieve filters from local storage, otherwise create new object
+    const filtersObj =
+      localStorage.getItem("updatesFilters") &&
+      !window.location.hash.split("#")[1]
+        ? JSON.parse(localStorage.getItem("updatesFilters"))
+        : { searchFilter: "", tagsFilter: ["info", "news"] };
 
-      lightbox.classList.remove("active");
-    });
+    function filterPosts() {
+      // pull values from website
+      filtersObj.searchFilter = filterSearch.value.toLowerCase();
 
-    const images = document.querySelectorAll("img");
+      filtersObj.tagsFilter = [];
+      filterButtons.forEach((btn) => {
+        if (btn.getAttribute("data-filter") === "false") {
+          filtersObj.tagsFilter.push(btn.getAttribute("data-value"));
+        }
+      });
 
-    images.forEach((image) => {
-      if (!image.getAttribute("data-disable-lightbox")) {
-        image.addEventListener("click", () => {
-          lightbox.classList.add("active");
+      // store object with values to local storage
+      localStorage.setItem("updatesFilters", JSON.stringify(filtersObj));
 
-          const img = document.createElement("img");
-          img.src = image.src;
+      // filter posts
+      for (let i = 0; i < postsArray.length; i++) {
+        if (displayPost(i)) {
+          postsArray[i].classList.remove("hidden");
+          postsArray[i].style.opacity = 1;
+        } else {
+          postsArray[i].style.opacity = 0.01;
 
-          while (lightbox.firstChild) {
-            lightbox.removeChild(lightbox.firstChild);
-          }
-
-          lightbox.appendChild(img);
-        });
+          // for animation in css
+          setTimeout(() => {
+            postsArray[i].classList.add("hidden");
+          }, 200);
+        }
       }
+    }
+
+    function displayPost(postIndex) {
+      const postContainsText =
+        titlesArray[postIndex].textContent
+          .toLowerCase()
+          .includes(filtersObj.searchFilter) ||
+        textsArray[postIndex].textContent
+          .toLowerCase()
+          .includes(filtersObj.searchFilter) ||
+        tagsArray[postIndex].textContent
+          .toLowerCase()
+          .includes(filtersObj.searchFilter) ||
+        datesArray[postIndex].textContent
+          .toLowerCase()
+          .includes(filtersObj.searchFilter);
+
+      const showInfo = tagsArray[postIndex].textContent
+        .toLowerCase()
+        .includes(filtersObj.tagsFilter[0]);
+      const showNews = tagsArray[postIndex].textContent
+        .toLowerCase()
+        .includes(filtersObj.tagsFilter[1]);
+
+      if (postContainsText && (showInfo || showNews)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    filterSearch.addEventListener("keyup", () => {
+      // when typing (also works with on screen keyboard)
+      filterPosts();
     });
+
+    // when x (clear) button pressed
+    filterSearch.addEventListener("search", () => {
+      filterPosts();
+    });
+
+    // info and news buttons
+    filterButtons.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        if (e.target.getAttribute("data-filter") === "false") {
+          e.target.setAttribute("data-filter", "true");
+        } else if (e.target.getAttribute("data-filter") === "true") {
+          e.target.setAttribute("data-filter", "false");
+        }
+
+        filterPosts();
+      });
+    });
+
+    // ===== if url with specified post id (#id) =====
+
+    if (window.location.hash.split("#")[1]) {
+      document
+        .querySelector('[id="' + window.location.hash.split("#")[1] + '"]')
+        .scrollIntoView(true);
+
+      localStorage.removeItem("updatesFilters");
+    } else {
+      // populate search field
+      filterSearch.value = filtersObj.searchFilter;
+      // toggle info and news buttons
+      filterButtons.forEach((btn) => {
+        if (
+          filtersObj.tagsFilter[0] === btn.getAttribute("data-value") ||
+          filtersObj.tagsFilter[1] === btn.getAttribute("data-value")
+        ) {
+          btn.setAttribute("data-filter", "false");
+        } else {
+          btn.setAttribute("data-filter", "true");
+        }
+      });
+
+      filterPosts();
+    }
   })
   .catch((err) => {
     console.error(err);
   });
+
+function createLightbox() {
+  const lightbox = document.createElement("div");
+  lightbox.id = "lightbox";
+
+  document.body.appendChild(lightbox);
+
+  lightbox.addEventListener("click", (e) => {
+    if (e.target !== e.currentTarget) return;
+
+    lightbox.classList.remove("active");
+  });
+
+  const images = document.querySelectorAll("img");
+
+  images.forEach((image) => {
+    if (!image.getAttribute("data-disable-lightbox")) {
+      image.addEventListener("click", () => {
+        lightbox.classList.add("active");
+
+        const img = document.createElement("img");
+        img.src = image.src;
+
+        while (lightbox.firstChild) {
+          lightbox.removeChild(lightbox.firstChild);
+        }
+
+        lightbox.appendChild(img);
+      });
+    }
+  });
+}
